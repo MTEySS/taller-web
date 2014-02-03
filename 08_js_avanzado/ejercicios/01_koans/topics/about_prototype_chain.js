24dd9f02cc2f64a7be82102847083d372ec93b35
opensas <opensas@gmail.com>
2014-02-03 03:56:17 -0300

    agregamos koans 1 y 2

---
+++ 08_js_avanzado/ejercicios/01_koans/topics/about_prototype_chain.js
@@ -0,0 +1,63 @@
+// demonstrate objects prototype chain
+
+// https://developer.mozilla.org/en/JavaScript/Guide/Inheritance_and_the_prototype_chain
+module("About Prototype Chain (topics/about_prototype_chain.js)");
+
+var father = {
+  b: 3,
+  c: 4
+};
+
+var child = Object.create(father);
+child.a = 1;
+child.b = 2;
+
+/*
+ * ---------------------- ---- ---- ----
+ *                      [a]  [b]  [c]
+ * ---------------------- ---- ---- ----
+ * [child]               1    2
+ * ---------------------- ---- ---- ----
+ * [father]                   3    4
+ * ---------------------- ---- ---- ----
+ * [Object.prototype]
+ * ---------------------- ---- ---- ----
+ * [null]
+ * ---------------------- ---- ---- ----
+ * */
+
+test("Is there an 'a' and 'b' own property on child?", function () {
+  equal(__, child.hasOwnProperty('a'), 'child.hasOwnProperty(\'a\')?');
+  equal(__, child.hasOwnProperty('b'), 'child.hasOwnProperty(\'b\')?');
+});
+
+test("Is there an 'a' and 'b' property on child?", function () {
+  equal(__, child.a, 'what is \'a\' value?');
+  equal(__, child.b, 'what is \'b\' value?');
+});
+
+test("If 'b' was removed, whats b value?", function () {
+  delete child.b;
+  equal(__, child.b, 'what is \'b\' value now?');
+});
+
+
+test("Is there a 'c' own property on child?", function () {
+  equal(__, child.hasOwnProperty('c'), 'child.hasOwnProperty(\'c\')?');
+});
+
+// Is there a 'c' own property on child? No, check its prototype
+// Is there a 'c' own property on child.[[Prototype]]? Yes, its value is...
+test("Is there a 'c' property on child?", function () {
+  equal(__, child.c, 'what is the value of child.c?');
+});
+
+
+// Is there a 'd' own property on child? No, check its prototype
+// Is there a 'd' own property on child.[[Prototype]]? No, check it prototype
+// child.[[Prototype]].[[Prototype]] is null, stop searching, no property found, return...
+test("Is there an 'd' property on child?", function () {
+  equal(__, child.d, 'what is the value of child.d?');
+});
+
+
---
+++ 08_js_avanzado/ejercicios/02_koans/koans/AboutFunctions.js
@@ -0,0 +1,100 @@
+describe("About Functions", function() {
+
+  it("should declare functions", function() {
+
+    function add(a, b) {
+      return a + b;
+    }
+
+    expect(add(1, 2)).toBe(FILL_ME_IN);
+  });
+
+  it("should know internal variables override outer variables", function () {
+    var message = "Outer";
+
+    function getMessage() {
+      return message;
+    }
+
+    function overrideMessage() {
+      var message = "Inner";
+      return message;
+    }
+
+    expect(getMessage()).toBe(FILL_ME_IN);
+    expect(overrideMessage()).toBe(FILL_ME_IN);
+    expect(message).toBe(FILL_ME_IN);
+  });
+
+  it("should have lexical scoping", function () {
+    var variable = "top-level";
+    function parentfunction() {
+      var variable = "local";
+      function childfunction() {
+        return variable;
+      }
+      return childfunction();
+    }
+    expect(parentfunction()).toBe(FILL_ME_IN);
+  });
+
+  it("should use lexical scoping to synthesise functions", function () {
+
+    function makeMysteryFunction(makerValue)
+    {
+      var newFunction = function doMysteriousThing(param)
+      {
+        return makerValue + param;
+      };
+      return newFunction;
+    }
+
+    var mysteryFunction3 = makeMysteryFunction(3);
+    var mysteryFunction5 = makeMysteryFunction(5);
+
+    expect(mysteryFunction3(10) + mysteryFunction5(5)).toBe(FILL_ME_IN);
+  });
+
+  it("should allow extra function arguments", function () {
+
+    function returnFirstArg(firstArg) {
+      return firstArg;
+    }
+
+    expect(returnFirstArg("first", "second", "third")).toBe(FILL_ME_IN);
+
+    function returnSecondArg(firstArg, secondArg) {
+      return secondArg;
+    }
+
+    expect(returnSecondArg("only give first arg")).toBe(FILL_ME_IN);
+
+    function returnAllArgs() {
+      var argsArray = [];
+      for (var i = 0; i < arguments.length; i += 1) {
+        argsArray.push(arguments[i]);
+      }
+      return argsArray.join(",");
+    }
+
+    expect(returnAllArgs("first", "second", "third")).toBe(FILL_ME_IN);
+  });
+
+  it("should pass functions as values", function () {
+
+    var appendRules = function (name) {
+      return name + " rules!";
+    };
+
+    var appendDoubleRules = function (name) {
+      return name + " totally rules!";
+    };
+
+    var praiseSinger = { givePraise: appendRules };
+    expect(praiseSinger.givePraise("John")).toBe(FILL_ME_IN);
+
+    praiseSinger.givePraise = appendDoubleRules;
+    expect(praiseSinger.givePraise("Mary")).toBe(FILL_ME_IN);
+
+  });
+});
